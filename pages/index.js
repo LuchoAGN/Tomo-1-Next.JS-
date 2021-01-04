f0eaf7676d0b85429d15f7f05e8070318604f4ee
Luis Alfredo García Nisperuza <studymedia2017@gmail.com>
2021-01-04 16:07:04 -0500

    init

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout'
+import Title from '../components/Title'
+
+export default function Home(){
+    return(
+        <Layout>
+            <Title>Home Page</Title>
+            <p>Arendamos NetJSno</p>
+        </Layout>
+    )
+}
